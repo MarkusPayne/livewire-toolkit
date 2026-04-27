3a0d24842fa87d52c6f9a4cb6748e77ee5d7be15
Mark Payne <markuspayne@gmail.com>
2026-04-27 14:52:10 -0400

    Add flatpickr styles and optional icon support for sidebar links

--- resources/views/components/sidebar/link.blade.php
+++ resources/views/components/sidebar/link.blade.php
@@ -1,8 +1,9 @@
 @props([
     'href',
+    'icon' => null
 ])
 <li
-    x-data="{
+        x-data="{
         active: false,
         get hasActiveChild() {
             return ! ! this.$el.querySelector('[data-current]')
@@ -11,11 +12,13 @@
             if (this.hasActiveChild) this.active = true
         },
     }"
-    x-on:livewire:navigated.window="if (hasActiveChild) active = true"
-    class="flex items-center gap-x-2 px-4 text-sm hover:text-sky-500">
-    <span class="flex items-center" :class="active && 'text-sky-700'">
-        {{ $icon ?? '' }}
-    </span>
+        x-on:livewire:navigated.window="if (hasActiveChild) active = true"
+        class="flex items-center gap-x-2 px-4 text-sm hover:text-sky-500">
+    @if ($icon)
+        <span class="flex items-center" :class="active && 'text-sky-700'">
+            {{ $icon }}
+        </span>
+    @endif
     <a href="{{ $href }}" wire:navigate class="flex items-center px-3 py-2 leading-5 text-gray-600 hover:text-sky-500">
         {{ $slot }}
     </a>

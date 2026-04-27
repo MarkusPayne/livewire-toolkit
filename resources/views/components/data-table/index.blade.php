1e3edf65aa4dc220576c0dd943232706956048bb
Mark Payne <markuspayne@gmail.com>
2026-04-27 18:38:00 -0400

    Remove unused `showExport` prop and related markup from data-table component

--- resources/views/components/data-table/index.blade.php
+++ resources/views/components/data-table/index.blade.php
@@ -1,9 +1,9 @@
 @props([
     'sortDir',
     'sortBy',
-    'showExport' => false,
+
 ])
-<div class="overflow-visible bg-white dark:bg-gray-800" x-data="{ showExport: @js($showExport) }">
+<div class="overflow-visible bg-white dark:bg-gray-800" >
     <div class="flex items-center justify-between pb-2">
         <div>
             <select wire:model.change.live="perPage"
@@ -16,11 +16,7 @@
         <div class="flex items-center justify-between gap-x-4 pb-3">
             {{ $extraHeading ?? null }}
 
-            <div x-show="showExport">
-                <svg class="inline-block h-5 w-5 cursor-pointer text-primary-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"></path>
-                </svg>
-            </div>
+
         </div>
     </div>
     <div class="table w-full border-collapse dark:bg-gray-700">

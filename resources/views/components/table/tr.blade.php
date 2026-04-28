f04d7956e7c65ddfe83a088dbbb39691653ec433
Mark Payne <markuspayne@gmail.com>
2026-04-28 15:17:09 -0400

    No changes made to `<x-table::tr>` component; remove unnecessary diff.

--- resources/views/components/table/tr.blade.php
+++ resources/views/components/table/tr.blade.php
@@ -4,7 +4,7 @@
 'textColor' => false,
 ])
 
-<div {{ $attributes->class(['table-row align-middle cursor-pointer items-center border-b border-t border-gray-200 dark:border-gray-700 dark:bg-gray-700 hover:bg-gray-100', 'text-['.$textColor.']' => $textColor, 'bg-red-300' => $hasError,'bg-green-400' => $hasSuccess]) }}>
+<div {{ $attributes->class(['table-row align-middle cursor-pointer items-center border-b border-gray-200 dark:border-gray-700 dark:bg-gray-700 hover:bg-gray-100', 'text-['.$textColor.']' => $textColor, 'bg-red-300' => $hasError,'bg-green-400' => $hasSuccess]) }}>
     {{ $slot }}
 
 </div>

fa28246a787bca42893a42e39634a75b0b0c673a
Mark Payne <markuspayne@gmail.com>
2026-04-28 14:09:47 -0400

    Refactor per-page selector to use `<x-toolkit::input.select>` component

--- resources/views/components/data-table/index.blade.php
+++ resources/views/components/data-table/index.blade.php
@@ -6,12 +6,12 @@
 <div class="overflow-visible bg-white dark:bg-gray-800" >
     <div class="flex items-center justify-between pb-2">
         <div>
-            <select wire:model.change.live="perPage"
-                    class="border-gray-300 rounded-md shadow-sm text-sm focus:border-primary-500 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-200 min-w-24">
+            <x-toolkit::input.select wire:model.change.live="perPage">
                 @foreach ($this->perPageOptions as $option)
                     <option value="{{ $option }}" wire:key="datatable-cnt-{{ $option }}">{{ $option }}</option>
                 @endforeach
-            </select>
+            </x-toolkit::input.select>
+
         </div>
         <div class="flex items-center justify-between gap-x-4 pb-3">
             {{ $extraHeading ?? null }}

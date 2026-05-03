1098c5f44dc31a62ec28f640522eaa4d2fb831e3
Mark Payne <markuspayne@gmail.com>
2026-05-03 13:36:29 -0400

    Update sidebar link hover and active state styles to use `sky` color theme instead of `primary`

--- resources/views/components/sidebar/link.blade.php
+++ resources/views/components/sidebar/link.blade.php
@@ -14,16 +14,16 @@
         },
     }"
         x-on:livewire:navigated.window="if (hasActiveChild) active = true"
-        {{ $attributes->merge(['class' => 'flex items-center gap-x-2 px-2 text-sm hover:text-primary-500 dark:hover:text-white']) }}
+        {{ $attributes->merge(['class' => 'flex items-center gap-x-2 px-2 text-sm hover:text-sky-500 dark:hover:text-white']) }}
 >
-    <span class="flex items-center" :class="active && 'text-primary-600! dark:text-gray-200!'">
+    <span class="flex items-center" :class="active && 'text-sky-600! dark:text-gray-200!'">
            @if ($icon)
             <span class="flex items-center" :class="active && 'text-sky-700'">
             {{ $icon }}
         </span>
         @endif
     </span>
-    <a href="{{ $href }}" wire:navigate class="flex items-center px-3 py-2 leading-5 dark:text-gray-200 text-gray-600 hover:text-primary-500 dark:hover:text-white" :class="active && 'text-primary-600! dark:text-gray-200!'">
+    <a href="{{ $href }}" wire:navigate class="flex items-center px-3 py-2 leading-5 dark:text-gray-200 text-gray-600 hover:text-sky-500 dark:hover:text-white" :class="active && 'text-sky-600! dark:text-gray-200!'">
         {{ $slot }}
     </a>
 </li>

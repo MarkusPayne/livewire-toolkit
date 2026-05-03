7f5982d451c7eb3f64825976932d47c45631d202
Mark Payne <markuspayne@gmail.com>
2026-05-03 11:45:10 -0400

    Refine modal structure and fix indentation for improved readability and consistency

--- resources/views/components/modal/large.blade.php
+++ resources/views/components/modal/large.blade.php
@@ -3,8 +3,8 @@
     'title' => null,
 ])
 @teleport('body')
-    <div>
-        <div
+<div>
+    <div
             x-data="{ showModal: false, name: '{{ $name }}' }"
             x-modelable="showModal"
             x-show="showModal"
@@ -16,22 +16,22 @@
             aria-modal="true"
             {{ $attributes }}
             class="fixed inset-0 z-70 overflow-y-auto text-left">
-            <div x-transition:enter.opacity class="fixed inset-0 bg-black/80"></div>
+        <div x-transition:enter.opacity class="fixed inset-0 bg-black/80"></div>
 
-            <div class="flex min-h-screen items-center justify-center p-4 text-center sm:items-center sm:p-0">
-                {{-- overflow-hidden --}}
-                <div class="relative my-8 w-full rounded-xs bg-white pt-5 text-left shadow-xl sm:mx-4 sm:max-w-(--breakpoint-2xl) sm:px-4 sm:py-6" x-trap.noscroll.inert="showModal">
-                    <div class="absolute top-2 right-2 block pt-2 pr-2">
+        <div class="flex min-h-screen items-center justify-center p-4 text-center sm:items-center sm:p-0">
+            {{-- overflow-hidden --}}
+            <div class="relative my-8 w-full rounded-xs bg-white pt-5 text-left shadow-xl sm:mx-4 sm:max-w-(--breakpoint-2xl) sm:px-4" x-trap.noscroll.inert="showModal">
+                <div class="absolute top-2 right-2 block pt-2 pr-2">
                         <span type="button" x-on:click="showModal = false" class="cursor-pointer rounded-xs bg-white text-red-500 hover:text-red-700">
                             <span class="sr-only">Close</span>
                             <x-toolkit::icon.close />
                         </span>
-                    </div>
-                    <div class="overflow-y-auto px-4 pt-4">
-                        {{ $slot }}
-                    </div>
+                </div>
+                <div class="overflow-y-auto p-4">
+                    {{ $slot }}
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endteleport

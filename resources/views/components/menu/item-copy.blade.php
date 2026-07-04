d6ce3c30deb1d07c92eeeb613d2008840f54697b
Mark Payne <markuspayne@gmail.com>
2026-07-04 11:34:58 -0400

    feat: add copy icon and copy menu item component

---
+++ resources/views/components/menu/item-copy.blade.php
@@ -0,0 +1,10 @@
+<x-toolkit::menu.close>
+    <x-toolkit::menu.item {{ $attributes }}>
+        <x-toolkit::icon.copy class="size-3 text-sky-600" />
+        @if ($slot->isEmpty())
+            Edit
+        @else
+            {{ $slot }}
+        @endif
+    </x-toolkit::menu.item>
+</x-toolkit::menu.close>
